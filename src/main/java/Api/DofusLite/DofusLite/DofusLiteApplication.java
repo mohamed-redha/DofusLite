eb789ed4148c9532a0e27788416da8fa6d340987
mohamed-redha <mohamed-redha@github.com>
2019-02-01 14:10:03 +0100

    Initial commit

---
+++ src/main/java/Api/DofusLite/DofusLite/DofusLiteApplication.java
@@ -0,0 +1,14 @@
+package Api.DofusLite.DofusLite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DofusLiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DofusLiteApplication.class, args);
+	}
+
+}
+
